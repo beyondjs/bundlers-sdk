3d0952afa67fbd80d144ceeb10e67f982692653e
Henry Box <14365005+henrybox@users.noreply.github.com>
2025-01-27 18:22:40 -0300

    feat: big bang

--- trash/processors/source/index.js
+++ trash/processors/source/index.js
@@ -1,2 +1,2 @@
-const Processor = require('@beyond-js/bundles-sdk/processor/source');
+const Processor = require('@beyond-js/bundlers-sdk/processor/source');
 module.exports = class extends Processor {};
